1b5333865309841535f236ceb8aa7486795fe7bc
salim aliyu musawa <salemaliyu1992@gmail.com>
2024-05-02 19:54:30 +0000

    element length

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """  return values with the appropriate types """
 
-from typing import Sequence, Tuple
+from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: Sequence[str]) -> Sequence[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence[str]]) -> List[
+        Tuple[Sequence[str], int]
+        ]:
+
     """
      return values with the appropriate types
     """
