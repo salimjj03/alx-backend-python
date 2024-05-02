1a2f59efc8c09cc41aa671f141f05889c5e4cca3
salim aliyu musawa <salemaliyu1992@gmail.com>
2024-05-02 12:30:42 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" this model takes a float n as argument and returns
+the floor of the float. """
+import math
+
+
+def floor(n: float) -> int:
+    """ this model takes a float n as argument and returns
+    the floor of the float. """
+    return math.floor(n)
