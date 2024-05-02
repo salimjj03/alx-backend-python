6e0a1f9b8e7849cd66b52df1752ef5ecd41aab39
salim aliyu musawa <salemaliyu1992@gmail.com>
2024-05-02 11:30:46 +0000

    variable_annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This modle takes a float a and a float b as
+arguments and returns their sum as a float. """
+
+def add(a: float, b: float) -> float:
+    """ This modle takes a float a and a float b as
+    arguments and returns their sum as a float. """
+    return a + b
