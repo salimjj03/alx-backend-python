d571e441852ed7d52dcb5e4fae62e125a79e127e
salim aliyu musawa <salemaliyu1992@gmail.com>
2024-05-02 12:56:13 +0000

    to string

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -3,7 +3,7 @@
 the string representation of the float. """
 
 
-def to_str(n: int) -> str:
+def to_str(n: float) -> str:
     """ This model float n as argument and returns
     the string representation of the float. """
 
