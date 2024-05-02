d456f03f571d3aeca0c35ef0c9693eec2f00aaf8
salim aliyu musawa <salemaliyu1992@gmail.com>
2024-05-02 11:49:58 +0000

    concat

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,7 @@
 """ This modle takes a float a and a float b as
 arguments and returns their sum as a float. """
 
+
 def add(a: float, b: float) -> float:
     """ This modle takes a float a and a float b as
     arguments and returns their sum as a float. """
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" This model takes a string str1 and a string str2 as
+arguments and returns a concatenated string. """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ This model takes a string str1 and a string str2 as
+    arguments and returns a concatenated string. """
+
+    return "{}{}".format(str1, str2)
