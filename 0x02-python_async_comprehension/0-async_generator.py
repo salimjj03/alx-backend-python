df74ec0162ac879eff4b1f495a04e61986e1f0a5
salim aliyu <salemaliyu1882@gmail.com>
2024-05-07 13:47:08 +0000

    async_generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,9 +7,10 @@ Use the random module.
 """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     coroutine called async_generator that takes no arguments.
     The coroutine will loop 10 times, each time asynchronously
